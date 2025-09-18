a38dab8261257b5358378eb246acbd6020ec5846
Jerel Miller <jerelmiller@gmail.com>
2025-09-18 15:00:48 -0600

    Export HttpHeadersLink and deprecate httpHeaders function

--- packages/apollo-angular/headers/src/index.ts
+++ packages/apollo-angular/headers/src/index.ts
@@ -1,18 +1,27 @@
 import { HttpHeaders } from '@angular/common/http';
 import { ApolloLink } from '@apollo/client/core';
 
+export class HttpHeadersLink extends ApolloLink {
+  constructor() {
+    super((operation, forward) => {
+      const { getContext, setContext } = operation;
+      const context = getContext();
+
+      if (context.headers) {
+        setContext({
+          ...context,
+          headers: new HttpHeaders(context.headers),
+        });
+      }
+
+      return forward(operation);
+    });
+  }
+}
+
+/**
+ * @deprecated Use `HttpHeadersLink` instead.
+ */
 export const httpHeaders = () => {
-  return new ApolloLink((operation: ApolloLink.Operation, forward: ApolloLink.ForwardFunction) => {
-    const { getContext, setContext } = operation;
-    const context = getContext();
-
-    if (context.headers) {
-      setContext({
-        ...context,
-        headers: new HttpHeaders(context.headers),
-      });
-    }
-
-    return forward(operation);
-  });
+  return new HttpHeadersLink();
 };
